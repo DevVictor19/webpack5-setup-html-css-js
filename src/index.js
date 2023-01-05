39702160e23233865c075ecd48bec4025c53e3b2
Antonio Victor <Antoniovictor12@live.com>
2023-01-05 20:17:43 -0300

    feat: estrutura finalizada

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { multiply } from "./js/multiply";
+import { sum } from "./js/sum";
+import "./styles/index.css";
+
+// run npm run build to see the minification and compression!
+
+const sumResult = sum(4, 20);
+const multResult = multiply(100, 100);
+
+console.log(sumResult);
+console.log(multResult);
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    bundle: path.resolve(__dirname, "src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name][contenthash].js",
+    clean: true,
+  },
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    watchFiles: ["./src/index.html"],
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modlues/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(svg|png|jpeg|jpg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack App",
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+    new CopyPlugin({
+      patterns: [{ from: "src/public", to: "public" }],
+    }),
+  ],
+};
